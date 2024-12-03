a7d1fae2da5dc4f264e4da91ddfc39bdffece0e5
dashingcoderrohan123 <anandrao23046@gmail.com>
2024-12-03 03:20:39 -0800

    Add files via upload

---
+++ lesson44.py
@@ -0,0 +1,9 @@
+#Write a program to display the weather in autumn and spring
+
+def weather_condition():
+    print("the weather is pleasant in",spring)
+    print("the weather is pleasant in",atumn)
+
+spring="atumn"
+atumn="spring"
+weather_condition()    
